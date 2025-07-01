b265e46d7572fbfa1be365bdf7c7455cca5a4cb6
L0wBly <arthurboutry2005@icloud.com>
2025-07-01 11:23:20 +0200

    fix

--- config.py
+++ config.py
@@ -3,7 +3,7 @@
 BUMP_CHANNEL_ID = 1374712467933888513    # ID du salon #bump
 BUMP_ROLE_ID = 1377230605309313085       # ID du rôle à ping
 BUMP_COOLDOWN = 2 * 60 * 60              # 2h en secondes
-REMIND_INTERVAL = 60 * 60                # 1h en secondes pour les rappels si pas bump
+REMIND_INTERVAL = 60 * 60 * 4               # 1h en secondes pour les rappels si pas bump
 DISBOARD_ID = 302050872383242240         # ID du bot Disboard
 
 GUESS_CHANNEL_ID = 1378780388377231501   # ID du salon #guess-the-number
